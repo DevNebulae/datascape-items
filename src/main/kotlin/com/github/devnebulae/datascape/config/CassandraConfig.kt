d2fc65899491e18f0b7d33d9a13795638b8f7901
Ivo Huntjens <gamerkeeperson@gmail.com>
2018-04-26 19:49:39 +0200

    Add CassandraConfig configuration class

---
+++ src/main/kotlin/com/github/devnebulae/datascape/config/CassandraConfig.kt
@@ -0,0 +1,38 @@
+package com.github.devnebulae.datascape.config
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
+import org.springframework.core.env.get
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration
+import org.springframework.data.cassandra.config.SchemaAction
+import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification
+
+@Configuration
+class CassandraConfig : AbstractCassandraConfiguration() {
+    @Autowired
+    private lateinit var environment: Environment
+
+    override fun getEntityBasePackages(): Array<String> {
+        return arrayOf("com.github.devnebulae.datascape.item")
+    }
+
+    @Bean
+    override fun getKeyspaceCreations(): MutableList<CreateKeyspaceSpecification> {
+        return mutableListOf(
+            CreateKeyspaceSpecification
+                .createKeyspace(keyspaceName)
+                .ifNotExists()
+                .withSimpleReplication()
+        )
+    }
+
+    override fun getKeyspaceName(): String {
+        return environment.get("spring.data.cassandra.keyspace-name")
+    }
+
+    override fun getSchemaAction(): SchemaAction {
+        return SchemaAction.RECREATE
+    }
+}
