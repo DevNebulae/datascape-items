b9b727474e1ead278154213c89a5c151eef1740f
Ivo Huntjens <gamerkeeperson+github@gmail.com>
2018-04-27 22:15:44 +0200

    Add new primary key field id and add @Id annotation to it

--- src/main/kotlin/com/github/devnebulae/datascape/item/Item.kt
+++ src/main/kotlin/com/github/devnebulae/datascape/item/Item.kt
@@ -3,25 +3,25 @@ package com.github.devnebulae.datascape.item
 import org.springframework.data.annotation.Id
 import org.springframework.data.cassandra.core.cql.Ordering
 import org.springframework.data.cassandra.core.cql.PrimaryKeyType
-import org.springframework.data.cassandra.core.mapping.Column
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn
 import org.springframework.data.cassandra.core.mapping.Table
 import java.io.Serializable
+import java.util.UUID
 
 @Table("item")
 data class Item(
-    @Id
-    @PrimaryKeyColumn(
-        name = "item_id",
-        ordering = Ordering.ASCENDING,
-        ordinal = 2,
-        type = PrimaryKeyType.PARTITIONED
-    )
     val itemId: Long,
 
-    @Column
     val name: String,
 
-    @Column
     val store: Long
-) : Serializable
+) : Serializable {
+    @Id
+    @PrimaryKeyColumn(
+        name = "id",
+        ordering = Ordering.ASCENDING,
+        ordinal = 1,
+        type = PrimaryKeyType.PARTITIONED
+    )
+    val id = UUID.randomUUID()
+}
