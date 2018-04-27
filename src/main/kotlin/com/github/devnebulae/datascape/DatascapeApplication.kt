f44b86b7cdbdb5e91d111e41aba60dccda91b79e
Ivo Huntjens <gamerkeeperson@gmail.com>
2018-04-27 15:06:41 +0200

    Add CommandLineRunner initializing the database

--- src/main/kotlin/com/github/devnebulae/datascape/DatascapeApplication.kt
+++ src/main/kotlin/com/github/devnebulae/datascape/DatascapeApplication.kt
@@ -1,10 +1,29 @@
 package com.github.devnebulae.datascape
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.github.devnebulae.datascape.item.Item
+import com.github.devnebulae.datascape.item.ItemDto
+import com.github.devnebulae.datascape.item.ItemRepository
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.core.io.ClassPathResource
 
 @SpringBootApplication
-class DatascapeApplication
+class DatascapeApplication {
+    @Bean
+    fun runner(repository: ItemRepository) = CommandLineRunner {
+        val mapper = ObjectMapper()
+        val items = mapper
+            .readValue<Map<String, ItemDto>>(ClassPathResource("items.json").file)
+            .map { Item(it.key.toLong(), it.value.name, it.value.store.toLong()) }
+
+
+        repository.saveAll(items)
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<DatascapeApplication>(*args)
