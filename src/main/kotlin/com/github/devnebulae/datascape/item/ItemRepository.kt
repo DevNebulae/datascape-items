4b379e91296ce5cea8dbf6d6623264451b61d727
Ivo Huntjens <gamerkeeperson@gmail.com>
2018-04-27 15:57:55 +0200

    Add @Repository annotation to ItemRepository interface

--- src/main/kotlin/com/github/devnebulae/datascape/item/ItemRepository.kt
+++ src/main/kotlin/com/github/devnebulae/datascape/item/ItemRepository.kt
@@ -1,5 +1,7 @@
 package com.github.devnebulae.datascape.item
 
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface ItemRepository : CrudRepository<Item, Long>
